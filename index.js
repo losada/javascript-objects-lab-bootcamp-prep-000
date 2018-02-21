d10934d5b12bfe118048191bd68cee64119f3347
Michael Losada <michael.losada@gmail.com>
2018-02-21 13:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(recipes, meal, ingredient) {
-  return Object.assign({},recipes,{[key]:value})
+  return Object.assign({},recipes,{[meal]:ingredient})
   
 }
 
